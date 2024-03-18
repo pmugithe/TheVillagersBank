0325bf50b5c30e1bc8d0456e5aa05776c0be2fab
PradeepMugithe <pmugithe@iu.edu>
2024-03-18 19:23:06 -0400

    Adding existing project to the repository

---
+++ accounts/accounts/src/main/java/com/thevillagersbank/accounts/AccountsApplication.java
@@ -0,0 +1,13 @@
+package com.thevillagersbank.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
