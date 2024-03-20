ab0e4b4c82b6a35703b856efee679c42b41767c6
PradeepMugithe <pmugithe@iu.edu>
2024-03-20 18:52:17 -0400

    New Code

--- accounts/accounts/src/main/java/com/thevillagersbank/accounts/controller/AccountController.java
+++ accounts/accounts/src/main/java/com/thevillagersbank/accounts/controller/AccountController.java
@@ -1,8 +1,12 @@
 package com.thevillagersbank.accounts.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController("/")
+@RestController
+// The default path for our application or stating point. 
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountController {
 
 	@GetMapping("/helloAccount")
